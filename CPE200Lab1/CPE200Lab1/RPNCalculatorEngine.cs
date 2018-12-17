08362b620326d92ddcb06b28a910838e87bca901
thrwKappu <thrwkappu@airmail.cc>
2018-12-17 23:15:50 +0700

    Added working Process(), passes all cases

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,12 +6,37 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
-        public string Process(string str)
+        public override string Process(string str)
         {
-            // your code here
-            return "E";
+            string _firstOp, _secondOp;
+            string[] _parts = str.Split(' ');
+
+            Stack<string> _nums = new Stack<string>();
+            for (int i = 0; i < _parts.Length; i++)
+            {
+                if (isNumber(_parts[i]))
+                {
+                    _nums.Push(_parts[i]);
+                }
+
+                if (isOperator(_parts[i]))
+                {
+                    if (_nums.Count <= 1)
+                    {
+                        return "E";
+                    }
+                    else
+                    {
+                        _secondOp = _nums.Pop();
+                        _firstOp = _nums.Pop();
+                        _nums.Push(calculate(_parts[i], _firstOp, _secondOp));
+                    }
+                }
+            }
+
+            return (_nums.Count == 1) ? _nums.Pop() : "E";
         }
     }
 }
